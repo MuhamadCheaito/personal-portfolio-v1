304461633c8c9c2f3b22fb87819382c9b3b6c7ca
MuhamadCheaito <cheaitomuhamad.92@gmail.com>
2021-12-16 20:06:01 +0200

    split data to single files

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -6,7 +6,7 @@
         Portfolio
       </div>
     </template>
-    <template #content>
+<template #content>
 <v-banner
   elevation="20"
   outlined
@@ -89,177 +89,14 @@ v-on="on"
 </template>
 
 <script>
+import projects from './data/projects_data'
 const BaseSection = () => import("./BaseSection.vue");
 
 export default {
   name: "Portfolio",
   data(){
     return {
-        projects:[{
-          name:"Inventory System",
-          desc:`The system provides information to efficiently manage the flow
-                  of products , effectively utilize people and equipment ,
-                  coordinate internal activities and communicate with customers.`,
-          imageUrl:require('@/assets/projects/inventory-system.png'),
-          type:"desktop",
-          tags: 'C# (WPF), MVVM, SQL Server',
-          status:'Completed'
-        },
-        {
-          name:"Car System",
-          desc:`This project was built based on the client’s requests where
-                  the user can login, add, edit and remove
-                  cars/suppliers/transits also converting from USD to Naira and much more.`,
-          imageUrl:require('@/assets/projects/car-project.png'),
-          type:"desktop",
-          tags: 'C# (Winforms), ADO.NET, SQL Server',
-          status:'Completed'
-        },
-        {
-          name:"MyReads App",
-          desc:`The Application is responsible for developing a habit of
-                  reading where you can add books by searching through the
-                  GoodReads API and add your reading progress to your profile.`,
-          imageUrl:require('@/assets/projects/my-reads-app.png'),
-          type:"mobile",
-          tags: 'Flutter, Dart, Firebase',
-          status:'Under Development'
-        },
-        {
-          name:"BMI Calculator UI",
-          desc:`The Application is responsible for taking a sample of
-                  information to calculate the BMI of the given sample,app is
-                  build based on a design from
-                  <v-btn
-                    text
-                    color="primary"
-                    href="https://dribbble.com/shots/4585382-Simple-BMI-Calculator"
-                    target="_blank"
-                    >dribbble.com</v-btn>`,
-          imageUrl:require('@/assets/projects/bmi-calculator.png'),
-          type:"mobile",
-          tags: 'Flutter (UI), Dart',
-          status:'Completed'
-        },
-        {
-          name:"Lab Al-hadi",
-          desc:`The website is built via WordPress my job is to support the
-                website by doing long to short time tasks improving it’s design
-                by using bit of CSS,adding plugins and also working on the
-                back-end.`,
-          imageUrl:require('@/assets/projects/lab-alhadi.png'),
-          type:"web",
-          tags: 'WordPress, PHP, MYSQL',
-          status:'Under Development'
-        },
-        {
-          name:"Movie Web App",
-          desc:`This static website is built for fun by using themoviedb.org API
-                it can search for movies and shows an overview when the user 
-                hovers over the movie's card.`,
-          imageUrl:require('@/assets/projects/movie-website.png'),
-          type:"web",
-          tags: 'HTML5, CSS, Vanilla JS',
-          status:'Completed'
-        },
-        {
-          name:"Split Landing Page",
-          desc:`This static website is built for fun by only html, css and 
-                javascript it when the user hovers the hovered content extends
-                in width with some cool transitions effects.`,
-          imageUrl:require('@/assets/projects/landing-page-template.png'),
-          type:"web",
-          tags: 'HTML5, CSS, Vanilla JS',
-          status:'Completed'
-        },
-        {
-          name:"Portfolio Challenge",
-          desc:`This static website is a challenge offered by a codementor.com
-          in which they provide screenshots of the design and the challenge is 
-          to obtain the same design.`,
-          imageUrl:require('@/assets/projects/protfolio-clone-challenge.png'),
-          type:"web",
-          tags: 'HTML5, CSS, Vanilla JS',
-          status:'Completed'
-        },
-        {
-          name:"Wikipedia Search Api",
-          desc:`This web application is built using angular,css
-          and bulma css framework, the user types something and the app 
-          fetches the results from wikipedia.com after clicking Return 
-          on the keyboard.`,
-          imageUrl:require('@/assets/projects/wikipedia-api.png'),
-          type:"web",
-          tags: 'Angular, Typescript, Bulma',
-          status:'Completed'
-        },
-        {
-          name:"Highlight Extractor",
-          desc:`This personal web application is built using flask and python,
-          using my notion account credentials this app helps me extract highlights
-          from pdf books and import them to my own notion template.`,
-          imageUrl:require('@/assets/projects/notion-extractor.png'),
-          type:"web",
-          tags: 'Python, Flask, Notion-py',
-          status:'Completed'
-        },
-        {
-          name:"Tic Tac Toe",
-          desc:`This small game was built for fun with angular where the user 
-                can play with no AI implemented.`,
-          imageUrl:require('@/assets/projects/tic-tac-toe.png'),
-          type:"web",
-          tags: 'Angular, Typescript, Material Design',
-          status:'Completed'
-        },
-        {
-          name:"Amazon Clone",
-          desc:`This web app was built with react in order to extend
-                my knowledge and learn react.`,
-          imageUrl:require('@/assets/projects/amazon-clone.png'),
-          type:"web",
-          tags: 'React, JSX',
-          status:'Under Development'
-        },
-        {
-          name:"Dating App",
-          desc:`This web app was built with angular and asp.net webapi where the user can 
-                register or login and his credentials are protected using JWT Token and 
-                also can date other users.`,
-          imageUrl:require('@/assets/projects/dating-app.png'),
-          type:"web",
-          tags: 'Angular, Asp.net WebApi',
-          status:'Under Development'
-        },
-        {
-          name:"BNB Investment App",
-          desc:`This web app was built with node.js and sqlite using sequelize where the admin 
-                login and setup an appoinetment for a donation process,for people to donate
-                BNB to his metamask credentials`,
-          imageUrl:require('@/assets/projects/node-metamask.png'),
-          type:"web",
-          tags: 'Nodejs, Web3, Passportjs, Bootstrap',
-          status:'Stopped'
-        },
-        {
-          name:"Personal Portoflio",
-          desc:`This web app was built with vuejs and vuetify it contains some cool animations 
-                and some reused components that can be used in future projects.`,
-          imageUrl:require('@/assets/projects/personal-portfolio.png'),
-          type:"web",
-          tags: 'Vue, Vuetify',
-          status:'Completed'
-        },
-        {
-          name:"Computer Design Page",
-          desc:`This static website was designed for a client on upwork.com using 
-                HTML and CSS only, and the client was satisfied with it.`,
-          imageUrl:require('@/assets/projects/upwork-simple.png'),
-          type:"web",
-          tags: 'HTML, CSS',
-          status:'Completed'
-        },
-        ],
+        projects: projects,
         choice:'desktop'
     }
   },
---
+++ src/views/data/skills_data.ts
@@ -0,0 +1,170 @@
+const data = [
+    {
+       skillTitle: 'FRONT-END',
+       skillDesc: `My peripheral knowledge in javascript frameworks 
+           front-end skills.`,
+       skillIcon:'mdi-desktop-mac-dashboard',
+       acquiredTechs:[
+         {
+             name:"html",
+             techImage:require('@/assets/icons/html-5.svg'),
+             color:"red darken-2",
+             progress:"95",
+         },
+         {
+             name:"css",
+             techImage:require('@/assets/icons/css3.svg'),
+             color:"blue darken-2",
+             progress:"90",
+         },
+          {
+             name:"scss",
+             techImage:require('@/assets/icons/sass.svg'),
+             color:"pink",
+             progress:"60",
+         },
+         {
+             name:"js",
+             techImage:require('@/assets/icons/javascript.svg'),
+             color:"yellow darken-2",
+             progress:"86",
+         },
+         {
+             name:"vue",
+             techImage:require('@/assets/icons/vue-js.svg'),
+             color:"green darken-2",
+             progress:"68",
+         },
+          {
+             name:"angular",
+             techImage:require('@/assets/icons/angular.svg'),
+             color:"red darken-2",
+             progress:"40",
+         },
+         {
+             name:"react",
+             techImage:require('@/assets/icons/react.svg'),
+             color:"blue darken-2",
+             progress:"50",
+         },
+         {
+             name:"bootstrap",
+             techImage:require('@/assets/icons/bootstrap-4.svg'),
+             color:"purple darken-2",
+             progress:"90",
+         },
+       ]
+    },
+    {
+       skillTitle: 'back-end',
+       skillDesc: `My acquired skills on both databases and back-end in
+           many techs.`,
+       skillIcon:'mdi-database-cog',
+       acquiredTechs:[
+         {
+             name:"csharp",
+             techImage:require('@/assets/icons/c-sharp.svg'),
+             color:"purple darken-2",
+             progress:"90",
+         },
+         {
+             name:"dotnet",
+             techImage:require('@/assets/icons/dotnet.svg'),
+             color:"blue",
+             progress:"70",
+         },
+          {
+             name:"mysql",
+             techImage:require('@/assets/icons/mysql.svg'),
+             color:"blue",
+             progress:"80",
+         },
+         {
+             name:"mongodb",
+             techImage:require('@/assets/icons/mongodb.svg'),
+             color:"green darken-1",
+             progress:"40",
+         },
+         {
+             name:"firebase",
+             techImage:require('@/assets/icons/firebase.svg'),
+             color:"yellow darken-2",
+             progress:"67",
+         },
+          {
+             name:"laravel",
+             techImage:require('@/assets/icons/laravel.svg'),
+             color:"red darken-2",
+             progress:"45",
+         },
+         {
+             name:"python",
+             techImage:require('@/assets/icons/python.svg'),
+             color:"yellow darken-1",
+             progress:"60",
+         },
+         {
+             name:"nodjs",
+             techImage:require('@/assets/icons/nodejs.svg'),
+             color:"green darken-2",
+             progress:"70",
+         },
+       ]
+    },
+    {
+       skillTitle: 'Other Skills',
+       skillDesc: `My acquired other skills in multi different technologies.`,
+       skillIcon:'mdi-pencil-ruler',
+       acquiredTechs:[
+         {
+             name:"git",
+             techImage:require('@/assets/icons/git.svg'),
+             color:"red darken-1",
+             progress:"60",
+         },
+         {
+             name:"flutter",
+             techImage:require('@/assets/icons/flutter.svg'),
+             color:"blue",
+             progress:"70",
+         },
+          {
+             name:"docker",
+             techImage:require('@/assets/icons/docker.svg'),
+             color:"blue darken-2",
+             progress:"40",
+         },
+         {
+             name:"kubernetes",
+             techImage:require('@/assets/icons/kubernetes.svg'),
+             color:"blue darken-1",
+             progress:"20",
+         },
+         {
+             name:"wordpress",
+             techImage:require('@/assets/icons/wordpress.svg'),
+             color:"blue darken-2",
+             progress:"85",
+         },
+          {
+             name:"php",
+             techImage:require('@/assets/icons/php.svg'),
+             color:"purple darken-2",
+             progress:"80",
+         },
+         {
+             name:"photoshop",
+             techImage:require('@/assets/icons/adobe-photoshop-2.svg'),
+             color:"blue darken-1",
+             progress:"75",
+         },
+         {
+             name:"java",
+             techImage:require('@/assets/icons/java.svg'),
+             color:"red darken-3",
+             progress:"50",
+         },
+       ]
+    },
+   ]
+export default data;
